8bd91a9b3dc82da3a24c06fb38b9ebc79135f22a
m-schuetz <mschuetz@potree.org>
2021-01-08 18:27:51 +0100

    ...

--- src/potree/renderFill.js
+++ src/potree/renderFill.js
@@ -7,13 +7,14 @@ import {RenderTarget} from "../core/RenderTarget.js";
 
 
 
-let _target = null;
+let _target_1 = null;
+let _target_2 = null;
 
-function getTarget(renderer){
-	if(_target === null){
+function getTarget1(renderer){
+	if(_target_1 === null){
 
 		let size = [128, 128, 1];
-		_target = new RenderTarget(renderer, {
+		_target_1 = new RenderTarget(renderer, {
 			size: size,
 			colorDescriptors: [{
 				size: size,
@@ -34,10 +35,36 @@ function getTarget(renderer){
 		});
 	}
 
-	return _target;
+	return _target_1;
 }
 
+function getTarget2(renderer){
+	if(_target_2 === null){
 
+		let size = [128, 128, 1];
+		_target_2 = new RenderTarget(renderer, {
+			size: size,
+			colorDescriptors: [{
+				size: size,
+				format: renderer.swapChainFormat,
+				usage: GPUTextureUsage.SAMPLED 
+					| GPUTextureUsage.COPY_SRC 
+					| GPUTextureUsage.COPY_DST 
+					| GPUTextureUsage.OUTPUT_ATTACHMENT,
+			}],
+			depthDescriptor: {
+				size: size,
+				format: "depth24plus-stencil8",
+				usage: GPUTextureUsage.SAMPLED 
+					| GPUTextureUsage.COPY_SRC 
+					| GPUTextureUsage.COPY_DST 
+					| GPUTextureUsage.OUTPUT_ATTACHMENT,
+			}
+		});
+	}
+
+	return _target_2;
+}
 
 
 
@@ -45,35 +72,43 @@ function getTarget(renderer){
 
 export function renderFill(renderer, pointcloud, camera){
 
-	let target = getTarget(renderer);
+	{ // PASS 1
+		let target = getTarget1(renderer);
 
-	let renderPassDescriptor = {
-		colorAttachments: [
-			{
-				attachment: target.colorAttachments[0].texture.createView(),
-				loadValue: { r: 0.2, g: 0.4, b: 0.3, a: 1.0 },
+		let renderPassDescriptor = {
+			colorAttachments: [
+				{
+					attachment: target.colorAttachments[0].texture.createView(),
+					loadValue: { r: 0.2, g: 0.4, b: 0.3, a: 1.0 },
+				},
+			],
+			depthStencilAttachment: {
+				attachment: target.depth.texture.createView(),
+				depthLoadValue: 1.0,
+				depthStoreOp: "store",
+				stencilLoadValue: 0,
+				stencilStoreOp: "store",
 			},
-		],
-		depthStencilAttachment: {
-			attachment: target.depth.texture.createView(),
-			depthLoadValue: 1.0,
-			depthStoreOp: "store",
-			stencilLoadValue: 0,
-			stencilStoreOp: "store",
-		},
-		sampleCount: 1,
-	};
+			sampleCount: 1,
+		};
 
-	const commandEncoder = renderer.device.createCommandEncoder();
-	const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+		const commandEncoder = renderer.device.createCommandEncoder();
+		const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
 
-	let pass = {commandEncoder, passEncoder, renderPassDescriptor};
+		let pass = {commandEncoder, passEncoder, renderPassDescriptor};
 
-	renderPoints(renderer, pass, pointcloud, camera);
+		renderPoints(renderer, pass, pointcloud, camera);
 
-	pass.passEncoder.endPass();
-	let commandBuffer = pass.commandEncoder.finish();
-	renderer.device.defaultQueue.submit([commandBuffer]);
+		pass.passEncoder.endPass();
+		let commandBuffer = pass.commandEncoder.finish();
+		renderer.device.defaultQueue.submit([commandBuffer]);
+	}
 
-	return target;
+	{ // PASS 2
+
+		
+
+	}
+
+	return getTarget1(renderer);
 }
