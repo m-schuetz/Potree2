55155711d2b50a98aedda5e45f4e046a19229f01
Markus Schütz <markus_schuetz@gmx.at>
2022-12-14 16:48:03 +0100

    ...

--- src/potree/Potree2Loader.js
+++ src/potree/Potree2Loader.js
@@ -30,9 +30,11 @@ export class Potree2Loader{
 	constructor(){
 		this.metadata = null;
 		this.metanodeMap = new Map();
+		this.nodeMap = new Map();
+		this.batches = new Map();
 	}
 
-	async loadNode(node){
+	async loadActualNode(node){
 
 		if(node.loading) return;
 
@@ -318,6 +320,49 @@ export class Potree2Loader{
 		node.loading = false;
 	}
 
+	async loadBatch(node){
+
+		let batch = node.batch;
+
+
+		if(batch.loading === true) return;
+		if(batch.loaded === true) return;
+		
+		console.log(`load batch ${batch.name}`)
+
+		batch.loading = true;
+
+		batch.nodes.sort((a, b) => {
+			return a.name.length - b.name.length;
+		});
+
+		for(let bmetanode of batch.nodes){
+			let node = this.nodeMap.get(bmetanode.name);
+			await this.loadActualNode(node);
+		}
+
+		batch.loading = false;
+		batch.loaded = true;
+	}
+
+	async loadNode(node){
+
+		let metanode = this.metanodeMap.get(node.name);
+
+		if(metanode.numVoxels > 0){
+			// load via batch
+
+			this.loadBatch(node);
+
+		}else if(metanode.numPoints > 0){
+			// load right here
+
+			this.loadActualNode(node);
+		}
+
+		
+	}
+
 	static async load(url){
 		let loader = new Potree2Loader();
 		loader.url = url;
@@ -368,22 +413,8 @@ export class Potree2Loader{
 		loader.octree = octree;
 		octree.material.init(octree);
 
-		// let root = new PointCloudOctreeNode("r");
-		// root.boundingBox = octree.boundingBox.clone();
-		// root.level = 0;
-		// root.nodeType = NodeType.NORMAL;
-		// root.spacing = octree.spacing;
-		// root.byteOffset = 0;
-		// root.octree = octree;
-
-		// sort by level
-
 		// LOAD NODES
-		let nodeMap = new Map();
-		// let metanodeMap = new Map();
 		for(let metanode of metadata.nodes){
-
-			// if(metanode.name.length > 2) continue;
 
 			let node = new PointCloudOctreeNode(metanode.name);
 			node.level = node.name.length - 1;
@@ -396,36 +427,40 @@ export class Potree2Loader{
 			node.octree = octree;
 			node.loaded = false;
 
-			nodeMap.set(node.name, node);
+			loader.nodeMap.set(node.name, node);
 			loader.metanodeMap.set(node.name, metanode);
 		}
 
 		// connect nodes
-		for(let [nodename, node] of nodeMap){
+		for(let [nodename, node] of loader.nodeMap){
 
 			if(nodename === "r") continue;
 
 			let parentName = nodename.substr(0, nodename.length - 1);
-			let parent = nodeMap.get(parentName);
+			let parent = loader.nodeMap.get(parentName);
 
 			let childIndex = Number(nodename.slice(-1));
 			parent.children[childIndex] = node;
 			node.parent = parent;
 		}
 
-		let root = nodeMap.get("r");
+		// BATCHES
+		for(let batch of metadata.batches){
+			loader.batches.set(batch.name, batch);
 
-		let nodes = Array.from(nodeMap.values());
+			for(let bmetanode of batch.nodes){
+				let node = loader.nodeMap.get(bmetanode.name);
+
+				node.batch = batch;
+			}
+		}
+
+		let root = loader.nodeMap.get("r");
+
+		let nodes = Array.from(loader.nodeMap.values());
 		nodes.sort((a, b) => {
 			return a.level - b.level;
 		});
-		
-		// load geometry
-		// root.traverse(async node => {
-		// for(let node of nodes){
-
-			
-		// }
 
 		octree.root = root;
 
